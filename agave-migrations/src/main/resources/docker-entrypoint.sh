9a9c9d97ef072f097be026fa992ed62f0f6bce73
Rion Dooley <deardooley@gmail.com>
2019-10-20 18:55:25 -0500

    Updating agave-migrations to build its container as part of the package lifecycle. Added creation of custom test db image during the `integration-test` by starting up a bitnami/mariadb:10.3 image, running the "migrate" task, and committing the container as a new `agave-mariadb:${project.verson}` image. Moved `agave-migrations` to the top of the submodule list after `agave-commons` so the generated db image will be available for use in all integration tests and we can avoid running migrations before every module integration test. Upgrading mysql connector in agave-commons to 8.0.7. Adding mariadb client connector 2.4.1.
    Former-commit-id: a4e6135a48d28b0e1c346f2fd382fd62fdd9da0b
    Former-commit-id: 6d5ca00d9c036a81deeb9897e48a75a889bb9641

--- agave-migrations/src/main/resources/docker-entrypoint.sh
+++ agave-migrations/src/main/resources/docker-entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # If no config file was included in the environment, use the default
-if [[ -z "$FLYWAY_CONFIG" ]]; then
+if [ -z "$FLYWAY_CONFIG" ]; then
   # use the flyway default location in case user just wants to drop
   # in the properties file and run the migration
-  if [[ -e "$FLYWAY_HOME/conf/flyway.conf" ]]; then
+  if [ -e "$FLYWAY_HOME/conf/flyway.conf" ]; then
       FLYWAY_CONFIG=$FLYWAY_HOME/conf/flyway.conf
-  elif [[ -e "$HOME/flyway.conf" ]]; then
+  elif [ -e "$HOME/flyway.conf" ]; then
       FLYWAY_CONFIG=$HOME/flyway.conf
-  elif [[ -e "flyway.properties" ]]; then
+  elif [ -e "flyway.properties" ]; then
       FLYWAY_CONFIG=$(pwd)/flyway.conf
   fi
-elif [[ ! -e "$FLYWAY_CONFIG" ]]; then 
+elif [ ! -e "$FLYWAY_CONFIG" ]; then 
 	echo "No config file present at $FLYWAY_CONFIG. Unable to run migrations!" 1>&2 
 fi
 
@@ -21,31 +21,31 @@ fi
 #
  
 # resolve the jdbc connection info from component variables if not specified directly
-if [[ -z $MYSQL_URL ]]; then
+if [ -z $MYSQL_URL ]; then
   # if a host is provided, we can build the url out of that, otherwise
   # we will fall back on the maven settings
   MYSQL_HOST=${MYSQL_HOST:-mysql}
   MYSQL_PORT=${MYSQL_PORT:-3306}
 
-  MYSQL_URL=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/'?zeroDateTimeBehavior=convertToNull&amp;sessionVariables=FOREIGN_KEY_CHECKS=0&amp;relaxAutoCommit=true&amp;rtinyInt1isBit=false'
+  MYSQL_URL=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/'?zeroDateTimeBehavior=convertToNull&relaxAutoCommit=true&tinyInt1isBit=false'
 fi
 
 sed -i 's#flyway.url=.*##g' $FLYWAY_CONFIG
 echo "flyway.url=$MYSQL_URL" >> $FLYWAY_CONFIG
 
-if [[ -n "$MYSQL_USERNAME" ]]; then
+if [ -n "$MYSQL_USERNAME" ]; then
   sed -i 's#flyway.user=.*#flyway.user='$MYSQL_USERNAME'#' $FLYWAY_CONFIG
 fi
 
-if [[ -n "$MYSQL_PASSWORD" ]]; then
+if [ -n "$MYSQL_PASSWORD" ]; then
   sed -i 's#flyway.password=.*#flyway.password='$MYSQL_PASSWORD'#' $FLYWAY_CONFIG
 fi
 
-if [[ -n "$MYSQL_DATABASE" ]]; then
+if [ -n "$MYSQL_DATABASE" ]; then
   sed -i 's#flyway.schemas=.*#flyway.schemas='$MYSQL_DATABASE'#' $FLYWAY_CONFIG
 fi
 
-if [[ -n "$BASELINE_VERSION" ]]; then
+if [ -n "$BASELINE_VERSION" ]; then
   sed -i 's#flyway.baselineVersion=.*#flyway.baselineVersion='$BASELINE_VERSION'#' $FLYWAY_CONFIG
 fi
 

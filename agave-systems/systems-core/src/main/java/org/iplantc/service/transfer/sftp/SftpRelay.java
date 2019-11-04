da8a0d4a2cd2b1d75bdb72505d5ba9251a738229
Eric Tanner <ertanner@gmail.com>
2019-11-04 15:20:40 -0800

    2.2.26 version.  Modified to work with the updated sftp.proto file
    Former-commit-id: 8dad49b120274bd245e535cacf1b7a6d208a27f4
    Former-commit-id: ee6769835a8a42bf8cfff49b27db4df49f7dc45a

--- agave-systems/systems-core/src/main/java/org/iplantc/service/transfer/sftp/SftpRelay.java
+++ agave-systems/systems-core/src/main/java/org/iplantc/service/transfer/sftp/SftpRelay.java
@@ -737,7 +737,7 @@ public final class SftpRelay implements RemoteDataClient {
                     // call the gRPC and get back a CopyLocalToRemoteResponse
                     SrvGetResponse srvGetResponseResponse = sftpClient.get(srvGetRequest);
 
-                    String response = srvGetResponseResponse.getResult();
+                    String response = srvGetResponseResponse.getError();
                     if (response.contains("Dialing") || response.contains("creating new client") || response.contains("opening source file")) {
                         throw new RemoteDataException(response);
                     } else {
@@ -963,7 +963,7 @@ public final class SftpRelay implements RemoteDataClient {
                     // call the gRPC and get back a SrvPutResponse
                     SrvPutResponse copyResponse = sftpClient.put(srvPutRequest);
 
-                    String response = copyResponse.getResult();
+                    String response = copyResponse.getError();
 
                     if (response.contains("Dialing") || response.contains("creating new client") || response.contains("opening source file")) {
                         throw new RemoteDataException(response);

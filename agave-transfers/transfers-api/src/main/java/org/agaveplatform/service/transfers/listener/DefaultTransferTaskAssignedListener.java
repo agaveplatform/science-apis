0243591c5344b04d364e5b9558e54ba3582d7591
Rion Dooley <deardooley@gmail.com>
2020-03-18 16:16:28 -0500

    Added cancellation listeners to DefaultTransferTaskAssignedListener so we can interrupte runnign tasks.

--- agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/listener/DefaultTransferTaskAssignedListener.java
+++ agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/listener/DefaultTransferTaskAssignedListener.java
@@ -16,11 +16,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 public class DefaultTransferTaskAssignedListener extends AbstractVerticle {
     private final Logger logger = LoggerFactory.getLogger(DefaultTransferTaskAssignedListener.class);
     private String eventChannel = "transfertask.assigned.*.*.*.*";
-//    private String eventChannel = "transfertask.*.get";
+    private HashSet<String> interruptedTasks = new HashSet<String>();
 
     public DefaultTransferTaskAssignedListener(Vertx vertx) {
         this(vertx, null);
@@ -42,8 +45,23 @@ public class DefaultTransferTaskAssignedListener extends AbstractVerticle {
             String dest = body.getString("dest");
             logger.info("Transfer task {} created: {} -> {}", uuid, source, dest);
             this.processTransferTask(body);
-        })
-        ;
+        });
+
+        bus.<JsonObject>consumer("transfertask.cancel.sync", msg -> {
+            JsonObject body = msg.body();
+            String uuid = body.getString("id");
+
+            logger.info("Transfer task {} cancel detected", uuid);
+            this.interruptedTasks.add(uuid);
+        });
+
+        bus.<JsonObject>consumer("transfertask.cancel.complete", msg -> {
+            JsonObject body = msg.body();
+            String uuid = body.getString("id");
+
+            logger.info("Transfer task {} cancel completion detected. Updating internal cache.", uuid);
+            this.interruptedTasks.remove(uuid);
+        });
     }
 
     protected String processTransferTask(JsonObject body) {
@@ -53,6 +71,7 @@ public class DefaultTransferTaskAssignedListener extends AbstractVerticle {
         String username = body.getString("owner");
         String tenantId = body.getString("tenantId");
         String protocol = null;
+        TransferTask bodyTask = new TransferTask(body);
 
         try {
 
@@ -61,34 +80,39 @@ public class DefaultTransferTaskAssignedListener extends AbstractVerticle {
                 srcUri = URI.create(source);
             } catch (Exception e) {
                 String msg = String.format("Unable to parse source uri %s for transfertask %s: %s",
-											source, uuid, e.getMessage());
+                        source, uuid, e.getMessage());
                 throw new RemoteDataSyntaxException(msg, e);
             }
-            // basic sanity check on uri again
-            if (RemoteDataClientFactory.isSchemeSupported(srcUri)) {
-                // if it's an "agave://" uri, look up the connection info, get a rdc, and process the remote
-                // file item
-                if (srcUri.getScheme().equalsIgnoreCase("agave")) {
-                    // pull the system out of the url. system id is the hostname in an agave uri
-                    RemoteSystem srcSystem = new SystemDao().findBySystemId(srcUri.getHost());
-                    // get a remtoe data client for the sytem
-                    RemoteDataClient client = srcSystem.getRemoteDataClient();
-                    // stat the remote path to check its type
-                    RemoteFileInfo fileInfo = client.getFileInfo(srcUri.getPath());
 
-                    // if the path is a file, then we can move it directly, so we raise an event telling the protocol
-                    // listener to move the file item
-                    if (fileInfo.isFile()) {
-                        // write to the protocol event channel. the uri is all they should need for this....
-                        // might need tenant id. not sure yet.
-                        if (interruptEvent(uuid, source, username, tenantId)) {
+            // check for task interruption
+            if (!isTaskInterrupted(bodyTask)) {
+                // basic sanity check on uri again
+                if (RemoteDataClientFactory.isSchemeSupported(srcUri)) {
+                    // if it's an "agave://" uri, look up the connection info, get a rdc, and process the remote
+                    // file item
+                    if (srcUri.getScheme().equalsIgnoreCase("agave")) {
+                        // pull the system out of the url. system id is the hostname in an agave uri
+                        RemoteSystem srcSystem = new SystemDao().findBySystemId(srcUri.getHost());
+                        // get a remtoe data client for the sytem
+                        RemoteDataClient client = srcSystem.getRemoteDataClient();
+                        // stat the remote path to check its type
+                        RemoteFileInfo fileInfo = client.getFileInfo(srcUri.getPath());
+
+                        // if the path is a file, then we can move it directly, so we raise an event telling the protocol
+                        // listener to move the file item
+                        if (fileInfo.isFile()) {
+                            // write to the protocol event channel. the uri is all they should need for this....
+                            // might need tenant id. not sure yet.
                             vertx.eventBus().publish("transfertask." + srcSystem.getType() + ".get",
-                                    "agave://" + srcSystem.getSystemId() + "/" + srcUri.getPath());
-                        }
-                    } else {
-                        if (interruptEvent(uuid, source, username, tenantId)) {
+                                        "agave://" + srcSystem.getSystemId() + "/" + srcUri.getPath());
+                        } else {
                             // path is a directory, so walk the first level of the directory
                             client.ls(srcUri.getPath())
+                                    .stream()
+                                    .takeWhile(rfi -> {
+                                        // check for an interrupt that comes while processing a potentially large dir listing
+                                        return !isTaskInterrupted(bodyTask);
+                                    })
                                     .forEach(childFileItem -> {
                                         // if it's a file, we can process this as we would if the original path were a file
                                         if (childFileItem.isFile()) {
@@ -115,17 +139,25 @@ public class DefaultTransferTaskAssignedListener extends AbstractVerticle {
                                     });
                         }
                     }
-                } else {
-                    if (interruptEvent(uuid, source, username, tenantId)) {
+                    // it's not an agave uri, so we forward on the raw uri as we know that we can
+                    // handle it from the wrapping if statement check
+                    else {
                         vertx.eventBus().publish("transfertask." + srcUri.getScheme() + ".get", source);
                     }
+                } else {
+                    // tell everyone else that you killed this task
+                    throw new InterruptedException(String.format("Transfer task %s interrupted due to cancel event", uuid));
                 }
             } else {
                 String msg = String.format("Unknown source schema %s for the transfertask %s",
-											srcUri.getScheme(), uuid);
+                        srcUri.getScheme(), uuid);
                 throw new RemoteDataSyntaxException(msg);
             }
-        } catch (Exception e) {
+        }
+        catch (InterruptedException e) {
+            logger.error(e.getMessage());
+        }
+        catch (Exception e) {
             logger.error(e.getMessage());
             JsonObject json = new JsonObject()
                     .put("cause", e.getClass().getName())
@@ -134,18 +166,35 @@ public class DefaultTransferTaskAssignedListener extends AbstractVerticle {
 
             getVertx().eventBus().publish("transfertask.error", json);
         }
+        finally {
+            // any interrupt involving this task will be processeda t this point, so acknowledge
+            // the task has been processed
+            if (isTaskInterrupted(bodyTask)) {
+                getVertx().eventBus().publish("transfertask.cancel.ack", body);
+            }
+        }
 
         return protocol;
     }
 
-    public boolean interruptEvent( String uuid, String source, String username, String tenantId ){
-        EventBus bus = vertx.eventBus();
-        bus.consumer(getEventChannel());
-        if ( bus.consumer("paused." + tenantId +"." + username + "." + uuid ).isRegistered()) {
-            logger.info("Transfer task paused {} created: {} -> {}", tenantId, uuid, source);
-            return true;
-        }
-        return false;
+    /**
+     * Checks whether the transfer task or any of its children exist in the list of
+     * interrupted tasks.
+     *
+     * @param transferTask the current task being checked from the running task
+     * @return true if the transfertask's uuid, parentTaskId, or rootTaskId are in the {@link #isTaskInterrupted(TransferTask)} list
+     */
+    public boolean isTaskInterrupted( TransferTask transferTask ){
+        return !(this.interruptedTasks.contains(transferTask.getUuid()) ||
+                this.interruptedTasks.contains(transferTask.getParentTaskId()) ||
+                this.interruptedTasks.contains(transferTask.getRootTaskId()));
+//        EventBus bus = vertx.eventBus();
+//        bus.consumer(getEventChannel());
+//        if ( bus.consumer("paused." + tenantId +"." + username + "." + uuid ).isRegistered()) {
+//            logger.info("Transfer task paused {} created: {} -> {}", tenantId, uuid, source);
+//            return true;
+//        }
+//        return false;
     }
 
     /**

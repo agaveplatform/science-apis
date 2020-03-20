62a8a7a33c78ec7f1b2ee8cf6d31c00434cb2b39
Rion Dooley <deardooley@gmail.com>
2020-03-19 19:10:57 -0500

    updating transfer task notification listener to propagate completion and cancel events to the notification queue. Adding transfer task notification listener to write vertx notification events to the legacy message queue so they can be processed by the legacy notifications api workers.

--- agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/listener/NotificationListener.java
+++ agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/listener/NotificationListener.java
@@ -6,6 +6,7 @@ import io.vertx.core.json.JsonObject;
 import org.agaveplatform.service.transfers.model.TransferTask;
 import org.iplantc.service.notification.dao.NotificationDao;
 import org.iplantc.service.notification.exceptions.NotificationException;
+import org.iplantc.service.notification.managers.NotificationManager;
 import org.iplantc.service.notification.model.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,73 +19,88 @@ public class NotificationListener extends AbstractVerticle {
 	@Override
 	public void start() {
 		EventBus bus = vertx.eventBus();
-		//final String err ;
-		bus.<JsonObject>consumer("transfertask.notification.*", msg -> {
+
+		// poc listener to show propagated notifications that woudl be sent to users
+		bus.<JsonObject>consumer("notification.*", msg -> {
 			JsonObject body = msg.body();
 
-			logger.info("Transfer task {} notification: {}", body.getString("id"), body.getString("message"));
-			TransferTask bodyTask = new TransferTask(body);
-			notification(bodyTask);
+			logger.info("{} notification event raised for {} {}: {}",
+					body.getString("event"),
+					body.getString("type"),
+					body.getString("uuid"),
+					body.encodePrettily());
+
+			NotificationManager.process( body.getString("id"), body.encode(), body.getString("owner"));
 		});
 
-		bus.<JsonObject>consumer("transfertask.cancel.sync", msg -> {
+		bus.<JsonObject>consumer("transfertask.cancel.completed", msg -> {
 			JsonObject body = msg.body();
 
-			logger.info("Transfer task {} notification: {}", body.getString("id"), body.getString("message"));
-			TransferTask bodyTask = new TransferTask(body);
-			String owner = bodyTask.getOwner();
-			String event = "Notification";
-			String uuid = bodyTask.getUuid();
-			boolean persistent = true;
-			String callback = "";
-			notification(event, callback, persistent, owner, uuid);
+			logger.info("Transfer task {} completed.", body.getString("id"));
+
+			getVertx().eventBus().publish("notification.cancelled", body);
+		});
+
+		bus.<JsonObject>consumer("transfertask.completed", msg -> {
+			JsonObject body = msg.body();
+
+			logger.info("Transfer task {} completed.", body.getString("id"));
+
+			getVertx().eventBus().publish("notification.completed", body);
+		});
+
+		bus.<JsonObject>consumer("transfertask.created", msg -> {
+			JsonObject body = msg.body();
+
+			logger.info("Transfer task {} created.", body.getString("id"));
+
 		});
 
 
 
 	}
 
-	public void notification( String event, String callback, boolean persistent, String owner, String uuid){
-		//TODO:  Write the nofication call here
-		try {
-			addNotification(event, callback, persistent, owner, uuid);
-		}
-		catch (Exception e){
-			logger.error(e.toString());
-		}
-	}
-
-
-
-	/**
-	 * Convenience endpoint to add a notification.
-	 *
-	 * @param event String notification event that will trigger the callback
-	 * @param callback A URL or email address that will be triggered by the event
-	 * @param persistent Whether this notification should expire after the first successful trigger
-	 */
-	@Transient
-	public void addNotification(String event, String callback, boolean persistent, String owner, String uuid)
-			throws NotificationException
-	{
-		Notification notification = new Notification(event, callback);
-		notification.setOwner(owner);
-		notification.setAssociatedUuid(uuid);
-		notification.setPersistent(persistent);
-
-		addNotification(notification, owner, uuid);
-	}
-	/**
-	 * Convenience endpoint to add a notification.
-	 *
-	 * @param notification A notification event to associate with this job. The current
-	 * jobs owner and uuid will be added to the notification.
-	 */
-	@Transient
-	public void addNotification(Notification notification, String owner, String uuid) throws NotificationException
-	{
-		notification.setOwner(owner);
-		notification.setAssociatedUuid(uuid);
-		new NotificationDao().persist(notification);
-	}
+//	public void notification( String event, String callback, boolean persistent, String owner, String uuid){
+//		//TODO:  Write the nofication call here
+//		try {
+//			addNotification(event, callback, persistent, owner, uuid);
+//		}
+//		catch (Exception e){
+//			logger.error(e.toString());
+//		}
+//	}
+//
+//
+//
+////	/**
+//	 * Convenience endpoint to add a notification.
+//	 *
+//	 * @param event String notification event that will trigger the callback
+//	 * @param callback A URL or email address that will be triggered by the event
+//	 * @param persistent Whether this notification should expire after the first successful trigger
+//	 */
+//	@Transient
+//	public void addNotification(String event, String callback, boolean persistent, String owner, String uuid)
+//			throws NotificationException
+//	{
+//		Notification notification = new Notification(event, callback);
+//		notification.setOwner(owner);
+//		notification.setAssociatedUuid(uuid);
+//		notification.setPersistent(persistent);
+//
+//		addNotification(notification, owner, uuid);
+//	}
+//	/**
+//	 * Convenience endpoint to add a notification.
+//	 *
+//	 * @param notification A notification event to associate with this job. The current
+//	 * jobs owner and uuid will be added to the notification.
+//	 */
+//	@Transient
+//	public void addNotification(Notification notification, String owner, String uuid) throws NotificationException
+//	{
+//		notification.setOwner(owner);
+//		notification.setAssociatedUuid(uuid);
+//		new NotificationDao().persist(notification);
+//	}
 }

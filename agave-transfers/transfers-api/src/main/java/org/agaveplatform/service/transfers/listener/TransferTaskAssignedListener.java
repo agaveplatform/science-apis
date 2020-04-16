c206560cfeec68bf347f75145d96f7e0977ad35f
Rion Dooley <deardooley@gmail.com>
2020-04-16 15:14:59 -0500

    Adding destination mkdirs calls to TransferTaskAssignedListener to ensure remote directory is present when the file transfer events fire.

--- agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/listener/TransferTaskAssignedListener.java
+++ agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/listener/TransferTaskAssignedListener.java
@@ -17,7 +17,8 @@ import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.util.HashSet;
-
+import java.util.List;
+import static org.agaveplatform.service.transfers.enumerations.MessageType.*;
 
 public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
     private final Logger logger = LoggerFactory.getLogger(TransferTaskAssignedListener.class);
@@ -87,7 +88,7 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
     protected String processTransferTask(JsonObject body) {
         String uuid = body.getString("uuid");
         String source = body.getString("source");
-//		String dest =  body.getString("dest");
+		String dest =  body.getString("dest");
         String username = body.getString("owner");
         String tenantId = body.getString("tenantId");
         String protocol = null;
@@ -96,8 +97,10 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
         try {
 
             URI srcUri;
+            URI destUri;
             try {
                 srcUri = URI.create(source);
+                destUri = URI.create(dest);
             } catch (Exception e) {
                 String msg = String.format("Unable to parse source uri %s for transfertask %s: %s",
                         source, uuid, e.getMessage());
@@ -114,9 +117,14 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
                         // pull the system out of the url. system id is the hostname in an agave uri
                         RemoteSystem srcSystem = new SystemDao().findBySystemId(srcUri.getHost());
                         // get a remtoe data client for the sytem
-                        RemoteDataClient client = srcSystem.getRemoteDataClient();
+                        RemoteDataClient srcClient = srcSystem.getRemoteDataClient();
+
+                        // pull the dest system out of the url. system id is the hostname in an agave uri
+                        RemoteSystem destSystem = new SystemDao().findBySystemId(destUri.getHost());
+                        RemoteDataClient destClient = destSystem.getRemoteDataClient();
+
                         // stat the remote path to check its type
-                        RemoteFileInfo fileInfo = client.getFileInfo(srcUri.getPath());
+                        RemoteFileInfo fileInfo = srcClient.getFileInfo(srcUri.getPath());
 
                         // if the path is a file, then we can move it directly, so we raise an event telling the protocol
                         // listener to move the file item
@@ -127,49 +135,68 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
                                         body);//"agave://" + srcSystem.getSystemId() + "/" + srcUri.getPath());
                         } else {
                             // path is a directory, so walk the first level of the directory
-                            client.ls(srcUri.getPath())
-                                    .stream()
-                                    .takeWhile(rfi -> {
-                                        // check for an interrupt that comes while processing a potentially large dir listing
-                                        return !isTaskInterrupted(bodyTask);
-                                    })
-                                    .forEach(childFileItem -> {
-                                        // if it's a file, we can process this as we would if the original path were a file
-                                        if (childFileItem.isFile()) {
-                                            // build the child paths
-                                            String childSource = body.getString("source") + "/" + childFileItem.getName();
-                                            String childDest = body.getString("dest") + "/" + childFileItem.getName();
+                            List<RemoteFileInfo> remoteFileInfoList = srcClient.ls(srcUri.getPath());
 
-                                            TransferTask transferTask = new TransferTask(childSource, childDest, tenantId);
-                                            transferTask.setTenantId(tenantId);
-                                            transferTask.setOwner(username);
-                                            transferTask.setParentTaskId(uuid);
-                                            if (StringUtils.isNotEmpty(body.getString("rootTask"))) {
-                                                transferTask.setRootTaskId(body.getString("rootTaskId"));
-                                            }
-                                            _doPublishEvent(MessageType.TRANSFERTASK_CREATED, transferTask.toJson());
+                            // if the directory is emnpty, mark as complete and exit
+                            if (remoteFileInfoList.isEmpty()) {
+                                _doPublishEvent(TRANSFER_COMPLETED, body);
+                            }
+                            // if there are contents, walk the first level, creating directories on the remote side
+                            // as we go to ensure that out of order processing by worker tasks can still succeed.
+                            else {
+                                // create the remote directory to ensure it's present when the transfers begin. This
+                                // also allows us to check for things like permissions ahead of time and save the
+                                // traversal in the event it's not allowed.
+                                destClient.mkdirs(destUri.getPath());
+
+                                for (RemoteFileInfo childFileItem: remoteFileInfoList) {
+                                    // allow for interrupts to come out of band
+                                    if (!isTaskInterrupted(bodyTask)) {
+                                        break;
+                                    }
+
+                                    // if it's a file, we can process this as we would if the original path were a file
+                                    if (childFileItem.isFile()) {
+                                        // build the child paths
+                                        String childSource = body.getString("source") + "/" + childFileItem.getName();
+                                        String childDest = body.getString("dest") + "/" + childFileItem.getName();
+
+                                        TransferTask transferTask = new TransferTask(childSource, childDest, tenantId);
+                                        transferTask.setTenantId(tenantId);
+                                        transferTask.setOwner(username);
+                                        transferTask.setParentTaskId(uuid);
+                                        if (StringUtils.isNotEmpty(body.getString("rootTask"))) {
+                                            transferTask.setRootTaskId(body.getString("rootTaskId"));
+                                        }
+                                        _doPublishEvent(MessageType.TRANSFERTASK_CREATED, transferTask.toJson());
 
 //                                            _doPublishEvent("transfertask." + srcSystem.getType(),
 //                                                    "agave://" + srcSystem.getSystemId() + "/" + srcUri.getPath() + "/" + childFileItem.getName());
-                                        }
-                                        // if a directory, then create a new transfer task to repeat this process,
-                                        // keep the association between this transfer task, the original, and the children
-                                        // in place for traversal in queries later on.
-                                        else {
-                                            // build the child paths
-                                            String childSource = body.getString("source") + "/" + childFileItem.getName();
-                                            String childDest = body.getString("dest") + "/" + childFileItem.getName();
+                                    }
+                                    // if a directory, then create a new transfer task to repeat this process,
+                                    // keep the association between this transfer task, the original, and the children
+                                    // in place for traversal in queries later on.
+                                    else {
+                                        // build the child paths
+                                        String childSource = body.getString("source") + "/" + childFileItem.getName();
+                                        String childDest = body.getString("dest") + "/" + childFileItem.getName();
 
-                                            TransferTask transferTask = new TransferTask(childSource, childDest, tenantId);
-                                            transferTask.setTenantId(tenantId);
-                                            transferTask.setOwner(username);
-                                            transferTask.setParentTaskId(uuid);
-                                            if (StringUtils.isNotEmpty(body.getString("rootTask"))) {
-                                                transferTask.setRootTaskId(body.getString("rootTaskId"));
-                                            }
-                                            _doPublishEvent(MessageType.TRANSFERTASK_CREATED, transferTask.toJson());
+//                                        // create the remote directory to ensure it's present when the transfers begin. This
+//                                        // also allows us to check for things like permissions ahead of time and save the
+//                                        // traversal in the event it's not allowed.
+//                                        boolean isDestCreated = destClient.mkdirs(destUri.getPath() + "/" + childFileItem.getName());
+
+                                        TransferTask transferTask = new TransferTask(childSource, childDest, tenantId);
+                                        transferTask.setTenantId(tenantId);
+                                        transferTask.setOwner(username);
+                                        transferTask.setParentTaskId(uuid);
+                                        if (StringUtils.isNotEmpty(body.getString("rootTask"))) {
+                                            transferTask.setRootTaskId(body.getString("rootTaskId"));
                                         }
-                                    });
+                                        _doPublishEvent(MessageType.TRANSFERTASK_CREATED, transferTask.toJson());
+                                    }
+                                }
+                            }
                         }
                     }
                     // it's not an agave uri, so we forward on the raw uri as we know that we can

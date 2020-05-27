5293101861f0a1f50760ad95b145f6901417c981
Eric Tanner <ertanner@gmail.com>
2020-05-27 09:12:09 -0700

    2.2.27/transfers-api
    Tests
    - Added TransferApplicationTest

---
+++ agave-transfers/transfers-api/src/test/java/org/agaveplatform/service/transfers/listener/TransferApplicationTest.java
@@ -0,0 +1,158 @@
+package org.agaveplatform.service.transfers.listener;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.PubSecKeyOptions;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
+import io.vertx.ext.jwt.JWTOptions;
+import io.vertx.junit5.Checkpoint;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import org.agaveplatform.service.transfers.BaseTestCase;
+import org.agaveplatform.service.transfers.model.TransferTask;
+import org.agaveplatform.service.transfers.resources.TransferApiVerticalTest;
+import org.agaveplatform.service.transfers.util.CryptoHelper;
+import org.iplantc.service.common.Settings;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static io.restassured.RestAssured.given;
+import static java.util.Arrays.asList;
+import static org.agaveplatform.service.transfers.enumerations.MessageType.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(VertxExtension.class)
+@DisplayName("TransfeeApplication tests")
+
+class TransferApplicationTest extends BaseTestCase {
+	private static final Logger log = LoggerFactory.getLogger(TransferApplicationTest.class);
+	private JWTAuth jwtAuth;
+	private Vertx vertx;
+
+	/**
+	 * Initializes the jwt auth options and the
+	 * @throws IOException when the key cannot be read
+	 */
+	private void initAuth() throws IOException {
+		JWTAuthOptions jwtAuthOptions = new JWTAuthOptions()
+				.addPubSecKey(new PubSecKeyOptions()
+						.setAlgorithm("RS256")
+						.setPublicKey(CryptoHelper.publicKey())
+						.setSecretKey(CryptoHelper.privateKey()));
+
+		jwtAuth = JWTAuth.create(vertx, jwtAuthOptions);
+	}
+
+	/**
+	 * Generates a JWT token to authenticate to the service. Token is signed using the
+	 * test private_key.pem and public_key.pem files in the resources directory.
+	 *
+	 * @param username Name of the test user
+	 * @return signed jwt token
+	 */
+	private String makeJwtToken(String username) {
+		// Add wso2 claims set
+		JsonObject claims = new JsonObject()
+				.put("http://wso2.org/claims/subscriber", username)
+				.put("http://wso2.org/claims/applicationid", "-9999")
+				.put("http://wso2.org/claims/applicationname", "agaveops")
+				.put("http://wso2.org/claims/applicationtier", "Unlimited")
+				.put("http://wso2.org/claims/apicontext", "/internal")
+				.put("http://wso2.org/claims/version", Settings.SERVICE_VERSION)
+				.put("http://wso2.org/claims/tier", "Unlimited")
+				.put("http://wso2.org/claims/keytype", "PRODUCTION")
+				.put("http://wso2.org/claims/usertype", "APPLICATION_USER")
+				.put("http://wso2.org/claims/enduser", username)
+				.put("http://wso2.org/claims/enduserTenantId", "-9999")
+				.put("http://wso2.org/claims/emailaddress", "testuser@example.com")
+				.put("http://wso2.org/claims/fullname", "Test User")
+				.put("http://wso2.org/claims/givenname", "Test")
+				.put("http://wso2.org/claims/lastname", "User")
+				.put("http://wso2.org/claims/primaryChallengeQuestion", "N/A")
+				.put("http://wso2.org/claims/role", "Internal/everyone,Internal/subscriber")
+				.put("http://wso2.org/claims/title", "N/A");
+
+		JWTOptions jwtOptions = new JWTOptions()
+				.setAlgorithm("RS256")
+				.setExpiresInMinutes(10_080) // 7 days
+				.setIssuer("transfers-api-integration-tests")
+				.setSubject(username);
+		return jwtAuth.generateToken(claims, jwtOptions);
+	}
+
+	@BeforeAll
+	public void setUpService() throws IOException {
+		// read in config options
+		initConfig();
+
+		// init the jwt auth used in the api calls
+		initAuth();
+	}
+
+	@Test
+	void main(Vertx vertx, VertxTestContext ctx) {
+		TransferTask tt = _createTestTransferTask();
+
+		RequestSpecification requestSpecification = new RequestSpecBuilder()
+				.addFilters(asList(new ResponseLoggingFilter(), new RequestLoggingFilter()))
+				.setBaseUri("http://localhost:" + 8085 + "/")
+				.build();
+
+		String response = given()
+				.spec(requestSpecification)
+				.header("X-JWT-ASSERTION-AGAVE_DEV", this.makeJwtToken(TEST_USERNAME))
+				.contentType(ContentType.JSON)
+				.body(tt.toJSON())
+				.when()
+				.post("api/transfers")
+				.then()
+				.assertThat()
+				.statusCode(201)
+				.extract()
+				.asString();
+
+		log.info("resopnse =" + response);
+		Checkpoint createdListenerCheckpoint = ctx.checkpoint();
+
+		ctx.verify(() -> {
+			AtomicInteger cnt = new AtomicInteger();
+			vertx.eventBus().addOutboundInterceptor(dc -> {
+				if (dc.message().body().equals(TRANSFERTASK_ASSIGNED)){
+					assertEquals(TRANSFERTASK_ASSIGNED, dc.message().body());
+				} else {
+					fail("wrong body");
+				}
+
+
+			});
+//		});
+//
+//		ctx.verify(() -> {
+			//AtomicInteger cnt = new AtomicInteger();
+			vertx.eventBus().addOutboundInterceptor(dc -> {
+				if (dc.message().body().equals(tt.toJson())){
+					assertEquals(TRANSFER_ALL, dc.message().body());
+				} else {
+					fail("wrong body");
+				}
+
+
+			});
+		});
+	}
+}
+

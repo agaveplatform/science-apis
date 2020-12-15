90d1eadf626772b4ae8a7020f7dfee5cded26961
Karen Liang <karen.liang95@gmail.com>
2020-12-14 21:00:37 -0800

    Updated config properties to use upper case snake case for flexibility to be set with environment variables

--- agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/TransferTaskConfigProperties.java
+++ agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/TransferTaskConfigProperties.java
@@ -4,17 +4,17 @@ package org.agaveplatform.service.transfers;
  * String literal constants representing the keys to the service's config file.
  */
 public class TransferTaskConfigProperties {
-    public static final String CONFIG_TRANSFERTASK_HTTP_PORT = "transfertask.http.port";
-    public static final String CONFIG_TRANSFERTASK_JWT_AUTH = "transfertask.jwt.auth";
-    public static final String CONFIG_TRANSFERTASK_JWT_VERIFY = "transfertask.jwt.verify";
-    public static final String CONFIG_TRANSFERTASK_JWT_PUBLIC_KEY = "transfertask.jwt.public_key";
-    public static final String CONFIG_TRANSFERTASK_JWT_PRIVATE_KEY= "transfertask.jwt.private_key";
-    public static final String CONFIG_TRANSFERTASK_DB_JDBC_URL = "transfertask.jdbc.url";
-    public static final String CONFIG_TRANSFERTASK_DB_JDBC_USERNAME = "transfertask.jdbc.username";
-    public static final String CONFIG_TRANSFERTASK_DB_JDBC_PASSWORD = "transfertask.jdbc.password";
-    public static final String CONFIG_TRANSFERTASK_DB_JDBC_DRIVER_CLASS = "transfertask.jdbc.driver_class";
-    public static final String CONFIG_TRANSFERTASK_DB_JDBC_MAX_POOL_SIZE = "transfertask.jdbc.max_pool_size";
-    public static final String CONFIG_TRANSFERTASK_DB_SQL_QUERIES_RESOURCE_FILE = "transfertask.sqlqueries.resource.file";
-    public static final String CONFIG_TRANSFERTASK_DB_QUEUE = "transfertask.db.queue";
-    public static final String TRANSFERTASK_MAX_ATTEMPTS = "transfertask.max.attempts";
+    public static final String CONFIG_TRANSFERTASK_HTTP_PORT = "TRANSFERTASK_HTTP_PORT";
+    public static final String CONFIG_TRANSFERTASK_JWT_AUTH = "TRANSFERTASK_JWT_AUTH";
+    public static final String CONFIG_TRANSFERTASK_JWT_VERIFY = "TRANSFERTASK_JWT_VERIFY";
+    public static final String CONFIG_TRANSFERTASK_JWT_PUBLIC_KEY = "TRANSFERTASK_JWT_PUBLIC_KEY";
+    public static final String CONFIG_TRANSFERTASK_JWT_PRIVATE_KEY= "TRANSFERTASK_JWT_PRIVATE_KEY";
+    public static final String CONFIG_TRANSFERTASK_DB_JDBC_URL = "TRANSFERTASK_DB_JDBC_URL";
+    public static final String CONFIG_TRANSFERTASK_DB_JDBC_USERNAME = "TRANSFERTASK_DB_JDBC_USERNAME";
+    public static final String CONFIG_TRANSFERTASK_DB_JDBC_PASSWORD = "TRANSFERTASK_DB_JDBC_PASSWORD";
+    public static final String CONFIG_TRANSFERTASK_DB_JDBC_DRIVER_CLASS = "TRANSFERTASK_DB_JDBC_DRIVER_CLASS";
+    public static final String CONFIG_TRANSFERTASK_DB_JDBC_MAX_POOL_SIZE = "TRANSFERTASK_DB_JDBC_MAX_POOL_SIZE";
+    public static final String CONFIG_TRANSFERTASK_DB_SQL_QUERIES_RESOURCE_FILE = "TRANSFERTASK_DB_SQL_QUERIES_RESOURCE_FILE";
+    public static final String CONFIG_TRANSFERTASK_DB_QUEUE = "TRANSFERTASK_DB_QUEUE";
+    public static final String TRANSFERTASK_MAX_ATTEMPTS = "TRANSFERTASK_MAX_ATTEMPTS";
 }

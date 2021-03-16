608a4a912cd59c6fe437759f9f734537352cde63
Rion Dooley <deardooley@gmail.com>
2021-03-15 19:41:43 -0500

    Refactored QuotaCheck instances to reduce information exposure needed to run the check and remove redundant checks for system existence, which would have had to have been determined prior to making this check.

--- agave-jobs/jobs-core/src/main/java/org/iplantc/service/jobs/managers/JobQuotaCheck.java
+++ agave-jobs/jobs-core/src/main/java/org/iplantc/service/jobs/managers/JobQuotaCheck.java
@@ -6,33 +6,47 @@ import org.iplantc.service.jobs.exceptions.QuotaViolationException;
 import org.iplantc.service.jobs.model.Job;
 import org.iplantc.service.systems.dao.SystemDao;
 import org.iplantc.service.systems.exceptions.SystemUnavailableException;
+import org.iplantc.service.systems.exceptions.SystemUnknownException;
 import org.iplantc.service.systems.model.BatchQueue;
 import org.iplantc.service.systems.model.ExecutionSystem;
+import org.iplantc.service.systems.model.RemoteSystem;
 import org.iplantc.service.systems.model.enumerations.SystemStatusType;
+
+import java.util.concurrent.ExecutorService;
 
 public class JobQuotaCheck implements QuotaCheck {
 	
 	private Job job;
+	private ExecutorService executionSystem;
+	private JobManager jobManager;
 	
-	public JobQuotaCheck(Job job)
-	{
-		this.job = job;
-	}
-	
-	@Override
-	public void check() throws QuotaViolationException, JobException, SystemUnavailableException
-	{
-		ExecutionSystem system = (ExecutionSystem) new SystemDao().findBySystemId(job.getSystem());
-		
-		if (system == null || !system.isAvailable() || !system.getStatus().equals(SystemStatusType.UP)) {
-			throw new SystemUnavailableException("Job execution system " + job.getSystem() + " is not available.");
+	public JobQuotaCheck() {}
+
+	/**
+	 * Identifies the {@link BatchQueue} to use in the quota calculation by looking at the queue named in the job,
+	 * then falling back to the {@link ExecutionSystem#getDefaultQueue()} if present.
+	 * @param job the job for which the queue is requested
+	 * @param executionSystem the job execution system
+	 * @return job queue, defalt system queue, or null if not found
+	 */
+	protected BatchQueue getJobQueue(Job job, ExecutionSystem executionSystem) {
+		BatchQueue jobQueue = executionSystem.getQueue(job.getBatchQueue());
+		if ( jobQueue == null ) {
+			if (executionSystem.getDefaultQueue() == null) {
+				jobQueue = new BatchQueue("default", 10L, 4.0);
+			}  else {
+				jobQueue = executionSystem.getDefaultQueue();
+			}
 		}
-		
-		// TODO: add check for batch queue on all hpc systems. this should always be false for them.
-        BatchQueue jobQueue = system.getQueue(job.getBatchQueue());
-        if ( jobQueue == null && system.getDefaultQueue() == null) {
-            jobQueue = new BatchQueue("default", new Long(10), new Double(4));
-        } 
+
+		return jobQueue;
+	}
+
+	@Override
+	public void check(Job job, ExecutionSystem executionSystem) throws QuotaViolationException, JobException
+	{
+		BatchQueue jobQueue = getJobQueue(job, executionSystem);
+        
         // todo end hack for queueless condor
 //        if ( jobQueue == null ) {
 //        	if (system.getDefaultQueue() == null && system.getScheduler() == SchedulerType.CONDOR) {
@@ -42,32 +56,27 @@ public class JobQuotaCheck implements QuotaCheck {
 //        	}
 //        } 
         
-		// verify the system quota first
-        if (jobQueue.getMaxJobs() == -1) {
-        	return;
-        }
-        // verify the system is not at capacity
-        else if (system.getMaxSystemJobs() > 0 && JobDao.countActiveJobsOnSystem(job.getSystem()) >= system.getMaxSystemJobs()) 
-		{
-			throw new QuotaViolationException(job.getSystem() + " is currently at capacity for new jobs.");
+		// if the job queue is not unbounded, ensure it doesn't violate the system limits
+        if (jobQueue.getMaxJobs() != -1) {
+			// verify the system is not at capacity
+			if (executionSystem.getMaxSystemJobs() > 0 && JobDao.countActiveJobsOnSystem(job.getSystem()) >= executionSystem.getMaxSystemJobs()) {
+				throw new QuotaViolationException(job.getSystem() + " is currently at capacity for new jobs.");
+			}
+			// verify the system queue is not at capacity
+			else if (jobQueue.getMaxJobs() > 0 && JobDao.countActiveJobsOnSystemQueue(job.getSystem(), job.getBatchQueue()) >= jobQueue.getMaxJobs()) {
+				throw new QuotaViolationException("System " + executionSystem.getSystemId() + " is currently at maximum capacity for "
+						+ "concurrent active jobs.");
+			}
+			// verify the user is not at system capacity
+			else if (executionSystem.getMaxSystemJobsPerUser() > 0 && JobDao.countActiveUserJobsOnSystem(job.getOwner(), job.getSystem()) >= executionSystem.getMaxSystemJobsPerUser()) {
+				throw new QuotaViolationException("User " + job.getOwner() + " has reached their quota for "
+						+ "concurrent active jobs on " + executionSystem.getSystemId());
+			}
+			// verify the user is not at queue capacity
+			else if (jobQueue.getMaxUserJobs() > 0 && JobDao.countActiveUserJobsOnSystemQueue(job.getOwner(), job.getSystem(), job.getBatchQueue()) >= jobQueue.getMaxUserJobs()) {
+				throw new QuotaViolationException("User " + job.getOwner() + " has reached their quota for "
+						+ "concurrent active jobs on the " + jobQueue.getName() + " queue of " + executionSystem.getSystemId());
+			}
 		}
-        // verify the system queue is not at capacity
-        else if (jobQueue.getMaxJobs() > 0 && JobDao.countActiveJobsOnSystemQueue(job.getSystem(), job.getBatchQueue()) >= jobQueue.getMaxJobs())
-        {
-        	throw new QuotaViolationException("System " + system.getSystemId() + " is currently at maximum capacity for "
-        			+ "concurrent active jobs.");
-        }
-        // verify the user is not at system capacity
-        else if (system.getMaxSystemJobsPerUser() > 0 && JobDao.countActiveUserJobsOnSystem(job.getOwner(), job.getSystem()) >= system.getMaxSystemJobsPerUser())
-        {
-        	throw new QuotaViolationException("User " + job.getOwner() + " has reached their quota for "
-        			+ "concurrent active jobs on " + system.getSystemId());
-        }
-        // verify the user is not at queue capacity
-        else if (jobQueue.getMaxUserJobs() > 0 && JobDao.countActiveUserJobsOnSystemQueue(job.getOwner(), job.getSystem(), job.getBatchQueue()) >= jobQueue.getMaxUserJobs())
-        {
-        	throw new QuotaViolationException("User " + job.getOwner() + " has reached their quota for "
-        			+ "concurrent active jobs on the " + jobQueue.getName() + " queue of " + system.getSystemId());
-        }
 	}
 }

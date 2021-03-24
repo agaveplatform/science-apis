b6ce45138ae72f6eade6b412257989e17e59a66c
Karen Liang <karen.liang95@gmail.com>
2021-03-24 16:55:24 -0700

    TransferTaskAssignedListener - forward transfers if the source and destination are on the same client for a simple copy request.

--- agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/listener/TransferTaskAssignedListener.java
+++ agave-transfers/transfers-api/src/main/java/org/agaveplatform/service/transfers/listener/TransferTaskAssignedListener.java
@@ -7,6 +7,7 @@ import org.agaveplatform.service.transfers.database.TransferTaskDatabaseService;
 import org.agaveplatform.service.transfers.enumerations.MessageType;
 import org.agaveplatform.service.transfers.enumerations.TransferStatusType;
 import org.agaveplatform.service.transfers.model.TransferTask;
+import org.agaveplatform.service.transfers.protocol.URLCopy;
 import org.apache.commons.lang.NotImplementedException;
 import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.iplantc.service.common.exceptions.AgaveNamespaceException;
@@ -39,9 +40,11 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
     private TransferTaskDatabaseService dbService;
 
     public TransferTaskAssignedListener() {super();}
+
     public TransferTaskAssignedListener(Vertx vertx) {
         super(vertx);
     }
+
     public TransferTaskAssignedListener(Vertx vertx, String eventChannel) {
         super(vertx, eventChannel);
     }
@@ -142,7 +145,7 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
         //Promise<Boolean> promise = Promise.promise();
         String uuid = body.getString("uuid");
         String source = body.getString("source");
-		String dest =  body.getString("dest");
+        String dest = body.getString("dest");
         String username = body.getString("owner");
         String tenantId = (body.getString("tenant_id"));
         String protocol = null;
@@ -155,6 +158,7 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
             URI srcUri;
             URI destUri;
             try {
+                // ensure it's a valid URI we can pass for parsing.
                 srcUri = URI.create(source);
                 destUri = URI.create(dest);
             } catch (Exception e) {
@@ -168,40 +172,46 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
             // If either one of them are then skipp the processing
             // otherwise process the task
             log.info("check to be sure root and parent are not null");
-           // if (assignedTransferTask.getRootTaskId() != null && assignedTransferTask.getParentTaskId() != null) {
-                // check for task interruption
-                log.info("Check for task interruption TransferTaskAssignedListener.processTransferTask");
-                log.info("Root task {}, Parent task {}", assignedTransferTask.getRootTaskId(), assignedTransferTask.getParentTaskId());
-                if (taskIsNotInterrupted(assignedTransferTask)) {
-                    log.trace("got past taskIsNotInterrupted");
-                    // basic sanity check on uri again
-                    // basic sanity check on uri again
-                    if (uriSchemeIsNotSupported(srcUri)) {
-                        String msg = String.format("Failing transfer task %s due to invalid scheme in source URI, %s",
-                                assignedTransferTask.getUuid(), assignedTransferTask.getSource());
-                        throw new RemoteDataSyntaxException(msg);
-                    } else if (uriSchemeIsNotSupported(destUri)) {
-                        String msg = String.format("Failing transfer task %s due to invalid scheme in destination URI, %s",
-                                assignedTransferTask.getUuid(), assignedTransferTask.getDest());
-                        throw new RemoteDataSyntaxException(msg);
-                    } else {
-                        log.trace("got into srcClient & destCleint");
-                        // get a remote data client for the source target
-                        srcClient = getRemoteDataClient(tenantId, username, srcUri);
-                        // get a remote data client for the dest target
-                        destClient = getRemoteDataClient(tenantId, username, destUri);
+            // if (assignedTransferTask.getRootTaskId() != null && assignedTransferTask.getParentTaskId() != null) {
+            // check for task interruption
+            log.info("Check for task interruption TransferTaskAssignedListener.processTransferTask");
+            log.info("Root task {}, Parent task {}", assignedTransferTask.getRootTaskId(), assignedTransferTask.getParentTaskId());
+            if (taskIsNotInterrupted(assignedTransferTask)) {
+                log.trace("got past taskIsNotInterrupted");
+                // basic sanity check on uri again
+                if (uriSchemeIsNotSupported(srcUri)) {
+                    String msg = String.format("Failing transfer task %s due to invalid scheme in source URI, %s",
+                            assignedTransferTask.getUuid(), assignedTransferTask.getSource());
+                    throw new RemoteDataSyntaxException(msg);
+                } else if (uriSchemeIsNotSupported(destUri)) {
+                    String msg = String.format("Failing transfer task %s due to invalid scheme in destination URI, %s",
+                            assignedTransferTask.getUuid(), assignedTransferTask.getDest());
+                    throw new RemoteDataSyntaxException(msg);
+                } else {
+                    log.trace("got into srcClient & destCleint");
+                    // get a remote data client for the source target
+                    srcClient = getRemoteDataClient(tenantId, username, srcUri);
+                    // get a remote data client for the dest target
+                    destClient = getRemoteDataClient(tenantId, username, destUri);
 
-                        // stat the remote path to check its type and existence
-                        log.info("stat the remote path to check its type and existence");
-                        RemoteFileInfo srcFileInfo = srcClient.getFileInfo(srcUri.getPath());
-                        log.info("Remote path was checked.");
+                    // stat the remote path to check its type and existence
+                    log.info("stat the remote path to check its type and existence");
+                    RemoteFileInfo srcFileInfo = srcClient.getFileInfo(srcUri.getPath());
+                    log.info("Remote path was checked.");
+
+                    // forward copy request directly to protocol if the source/destination is the same
+                    if (srcClient.equals(destClient)) {
+                        _doPublishEvent(TRANSFER_ALL, assignedTransferTask.toJson());
+
+                    } else {
+
                         // if the path is a file, then we can move it directly, so we raise an event telling the protocol
                         // listener to move the file item
                         if (srcFileInfo.isFile()) {
                             log.info("srcFileInfo is a file.");
                             // but first, we udpate the transfer task status to ASSIGNED
                             assignedTransferTask.setStatus(TransferStatusType.ASSIGNED);
-                            _doPublishEvent(TRANSFER_ALL, assignedTransferTask.toJson() );
+                            _doPublishEvent(TRANSFER_ALL, assignedTransferTask.toJson());
 
 //                            getDbService().updateStatus(tenantId, uuid, TransferStatusType.ASSIGNED.name(), updateResult -> {
 //                                if (updateResult.succeeded()) {
@@ -236,6 +246,7 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
                                 // but first, we udpate the transfer task status to ASSIGNED
                                 getDbService().updateStatus(tenantId, uuid, TransferStatusType.COMPLETED.name(), updateResult -> {
                                     if (updateResult.succeeded()) {
+
                                         log.debug("Assigning single file transfer task {} to the TRANSFER_COMPLETED queue as the remote source directory path is empty. And tenant id is {}", uuid, tenantId);
                                         // write to the completed event channel.
                                         assignedTransferTask.setStatus(TransferStatusType.COMPLETED);
@@ -264,7 +275,8 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
                                 // if the created transfertask does not have a rootTask, it is the rootTask of all
                                 // the child tasks we create processing the directory listing
                                 final String rootTaskId = assignedTransferTask.getRootTaskId() == null ?
-                                    assignedTransferTask.getUuid() : assignedTransferTask.getRootTaskId();
+                                        assignedTransferTask.getUuid() : assignedTransferTask.getRootTaskId();
+
 
                                 // create a new executor pool so we don't block the event thread with the potentially large number of network operations
                                 MutableBoolean ongoing = new MutableBoolean(true);
@@ -291,6 +303,7 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
                                                     childTransferTask.setStatus(childFileItem.isDirectory() ? TransferStatusType.CREATED : TransferStatusType.ASSIGNED);
 
                                                     getDbService().createOrUpdateChildTransferTask(tenantId, childTransferTask, childResult -> {
+
                                                         String fileItemType = childFileItem.isFile() ? "file" : "directory";
                                                         if (childResult.succeeded()) {
 
@@ -360,6 +373,7 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
                                         // update the original task now that we've processed all of its children
                                         assignedTransferTask.setStatus(TransferStatusType.ASSIGNED);
                                         assignedTransferTask.setStartTime(Instant.now());
+
                                         getDbService().update(tenantId, uuid, assignedTransferTask, updateResult -> {
                                             if (updateResult.succeeded()) {
                                                 log.debug("Updated parent transfer task {} to ASSIGNED after processing all its children.", uuid);
@@ -387,31 +401,30 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
                             }
                         }
                     }
-                } else {
-                    // task was interrupted, so don't attempt a retry
-                    log.info("Skipping processing of child file items for transfer tasks {} due to interrupt event.", uuid);
-                    _doPublishEvent(MessageType.TRANSFERTASK_CANCELED_ACK, body);
-                    handler.handle(Future.succeededFuture(false));
                 }
+            } else {
+                // task was interrupted, so don't attempt a retry
+                log.info("Skipping processing of child file items for transfer tasks {} due to interrupt event.", uuid);
+                _doPublishEvent(MessageType.TRANSFERTASK_CANCELED_ACK, body);
+                handler.handle(Future.succeededFuture(false));
+            }
             //}
-        }
-        catch (RemoteDataSyntaxException e) {
+        } catch (RemoteDataSyntaxException e) {
             String message = String.format("Failing transfer task %s due to invalid source syntax. %s", uuid, e.getMessage());
             log.error(message);
             doHandleFailure(e, message, body, handler);
-        }
-        catch (Throwable e) {
+        } catch (Throwable e) {
             String message = String.format("Caught a general exception. %s  %s", uuid, e.getMessage());
             log.error(message);
             doHandleError(e, e.getMessage(), body, handler);
-        }
-        finally {
+        } finally {
             // cleanup the remote data client connections
             try { if (srcClient != null) srcClient.disconnect(); } catch (Exception ignored) {}
             try { if (destClient != null) destClient.disconnect(); } catch (Exception ignored) {}
         }
         handler.handle(Future.succeededFuture(true));
     }
+
 
     public TransferTaskDatabaseService getDbService() {
         return dbService;
@@ -426,19 +439,23 @@ public class TransferTaskAssignedListener extends AbstractTransferTaskListener {
      * agave {@link RemoteSystem} URI vs externally accesible URI. Tenancy is honored.
      * @param tenantId the tenant whithin which any system lookups should be made
      * @param username the user for whom the system looks should be made
-     * @param target the uri from which to parse the system info
+     * @param target   the uri from which to parse the system info
      * @return a new instance of a {@link RemoteDataClient} for the given {@code target}
-     * @throws SystemUnknownException if the sytem is unknown
-     * @throws AgaveNamespaceException if the URI does match any known agave uri pattern
+     * @throws SystemUnknownException    if the sytem is unknown
+     * @throws AgaveNamespaceException   if the URI does match any known agave uri pattern
      * @throws RemoteCredentialException if the credentials for the system represented by the URI cannot be found/refreshed/obtained
-     * @throws PermissionException when the user does not have permission to access the {@code target}
-     * @throws FileNotFoundException when the remote {@code target} does not exist
-     * @throws RemoteDataException when a connection cannot be made to the {@link RemoteSystem}
-     * @throws NotImplementedException when the schema is not supported
+     * @throws PermissionException       when the user does not have permission to access the {@code target}
+     * @throws FileNotFoundException     when the remote {@code target} does not exist
+     * @throws RemoteDataException       when a connection cannot be made to the {@link RemoteSystem}
+     * @throws NotImplementedException   when the schema is not supported
      */
     protected RemoteDataClient getRemoteDataClient(String tenantId, String username, URI target) throws NotImplementedException, SystemUnknownException, AgaveNamespaceException, RemoteCredentialException, PermissionException, FileNotFoundException, RemoteDataException {
         TenancyHelper.setCurrentTenantId(tenantId);
         return new RemoteDataClientFactory().getInstance(username, null, target);
+    }
+
+    protected URLCopy getUrlCopy(RemoteDataClient srcClient, RemoteDataClient destClient) {
+        return new URLCopy(srcClient, destClient, getVertx(), getRetryRequestManager());
     }
 
 }

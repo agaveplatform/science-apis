0fab44be91e7ef77fdd13b8e5d16096e1d499a77
Rion Dooley <deardooley@gmail.com>
2021-04-19 02:19:54 -0500

    Rolling back the previous migration change so we can roll forward on existing migrated databases.

--- agave-migrations/src/main/resources/db/migration/V2.2.27.10__Rename_tag_event_table.sql
+++ agave-migrations/src/main/resources/db/migration/V2.2.27.10__Rename_tag_event_table.sql
@@ -1,8 +1,10 @@
 ###############################################################
-# Migration: V2.1.9.2__Rename_tag_event_table.sql
+# Migration: V2.2.27.10__Rename_tag_event_table.sql
 #
 # Renaming tag_event table to tagevents for consistency with other
-# history tables
+# history tables. This fixes a bug in the V2.1.9.2__Rename_tag_event_table.sql
+# migration where the schema was hard coded inot the query
+# definition.
 # 									   
 # Database changes:
 #
@@ -21,8 +23,7 @@
 SELECT Count(*)
 INTO @exists
 FROM information_schema.tables 
-WHERE table_schema = 'agave-api'
-    AND table_type = 'BASE TABLE'
+WHERE table_type = 'BASE TABLE'
     AND table_name = 'tag_event';
 
 SET @query = If(@exists>0,

a4a2d8e0ad56d4fbc9b0472b64c648f5cfd91cc3
Karen Liang <karen.liang95@gmail.com>
2021-06-02 17:57:19 -0700

    Pushing current progress on FileTransferNotificationIT;

--- agave-files/files-core/src/test/java/org/iplantc/service/io/notifications/FileTransferNotificationIT.java
+++ agave-files/files-core/src/test/java/org/iplantc/service/io/notifications/FileTransferNotificationIT.java
@@ -18,19 +18,17 @@ import org.iplantc.service.io.model.QueueTask;
 import org.iplantc.service.io.model.enumerations.FileEventType;
 import org.iplantc.service.io.model.enumerations.StagingTaskStatus;
 import org.iplantc.service.notification.dao.NotificationDao;
-import org.iplantc.service.notification.exceptions.NotificationException;
 import org.iplantc.service.notification.model.Notification;
 import org.iplantc.service.systems.dao.SystemDao;
 import org.iplantc.service.systems.model.StorageSystem;
-import org.json.JSONException;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.quartz.SchedulerException;
 import org.testng.Assert;
 
 import java.net.URI;
+import java.time.Instant;
 import java.util.List;
-import java.util.UUID;
 
 
 @ExtendWith(VertxExtension.class)
@@ -48,13 +46,14 @@ public class FileTransferNotificationIT extends BaseTestCase {
 
 
     @BeforeAll
-    protected void beforeClass(Vertx vertx, VertxTestContext ctx) throws Exception {
+    protected void beforeClass() throws Exception {
         super.beforeClass();
 
         clearSystems();
         clearLogicalFiles();
 
-        destPath = String.format("/home/%s/%s/%s", SYSTEM_OWNER, UUID.randomUUID().toString(), LOCAL_TXT_FILE_NAME);
+//        destPath = String.format("agave://sftp/home/%s/%s/%s", SYSTEM_OWNER, UUID.randomUUID().toString(), LOCAL_TXT_FILE_NAME);
+        destPath = String.format("agave://sftp/tmp");
         httpUri = new URI("https://httpd:8443/public/test_upload.bin");
 
 
@@ -69,13 +68,13 @@ public class FileTransferNotificationIT extends BaseTestCase {
 
 
     @AfterAll
-    protected void afterClass(Vertx vertx, VertxTestContext ctx) throws Exception {
+    protected void afterClass() throws Exception {
         clearSystems();
         clearLogicalFiles();
     }
 
     @BeforeEach
-    protected void setUp(Vertx vertx, VertxTestContext ctx) throws Exception {
+    protected void setUp() throws Exception {
         clearLogicalFiles();
 
         file = new LogicalFile(SYSTEM_OWNER, system, httpUri, destPath);
@@ -100,46 +99,76 @@ public class FileTransferNotificationIT extends BaseTestCase {
                 filesListenerDeploymentCheckpoint.flag();
 
                 try {
-                    vertx.eventBus().request("TRANSFER_COMPLETED", new JsonObject(file.toJSON()), response -> {
+                    vertx.eventBus().send("transfer.completed", getTransferTask(file, "COMPLETED"), response -> {
                         if (response.succeeded()){
                             ctx.verify(() -> {
-                                JsonObject jsonLogicalFile = (JsonObject) response.result();
-
-                                Assertions.assertNotNull(jsonLogicalFile, "Returned task is not null");
-                                Assertions.assertEquals(jsonLogicalFile.getString("source"), file.getSourceUri(), "Returned task source is equivalent to the original source");
-                                Assertions.assertEquals(jsonLogicalFile.getString("path"), file.getPath(), "Returned task dest is equivalent to the original dest");
-                                Assertions.assertEquals(jsonLogicalFile.getString("owner"), SYSTEM_OWNER, "Returned task owner is equivalent to the jwt user");
-                                Assertions.assertEquals(jsonLogicalFile.getString("tenantId"), file.getTenantId(), "Returned task tenant id is equivalent to the jwt tenant id");
-                                Assertions.assertEquals(jsonLogicalFile.getString("status"), "COMPLETED", "Returned task status is completed on successful transfer");
+//                                JsonObject jsonLogicalFile = (JsonObject) response.result();
+//
+//                                Assertions.assertNotNull(jsonLogicalFile, "Returned task is not null");
+//                                Assertions.assertEquals(jsonLogicalFile.getString("source"), file.getSourceUri(), "Returned task source is equivalent to the original source");
+//                                Assertions.assertEquals(jsonLogicalFile.getString("path"), file.getPath(), "Returned task dest is equivalent to the original dest");
+//                                Assertions.assertEquals(jsonLogicalFile.getString("owner"), SYSTEM_OWNER, "Returned task owner is equivalent to the jwt user");
+//                                Assertions.assertEquals(jsonLogicalFile.getString("tenantId"), file.getTenantId(), "Returned task tenant id is equivalent to the jwt tenant id");
+//                                Assertions.assertEquals(jsonLogicalFile.getString("status"), "COMPLETED", "Returned task status is completed on successful transfer");
 
                                 requestCheckpoint.flag();
+
+                                //Verify that the notification sent onto the queue is correct
+                                List<Notification> notifications = notificationDao.getActiveForAssociatedUuidAndEvent(file.getTransferUuid(), FileEventType.STAGING_COMPLETED.name());
+                                Assert.assertFalse(notifications.isEmpty(), "No notifications found for status " + FileEventType.STAGING_COMPLETED.name());
+                                Assert.assertEquals(notifications.size(), 1, "Wrong number of notifications returned for status " + FileEventType.STAGING_COMPLETED.name());
+
+                                Notification n = notifications.get(0);
+                                Assert.assertTrue(n.getUuid() == file.getTransferUuid(), "Notification for transfer " + file.getTransferUuid() + " was successfully created");
+                                Assert.assertTrue(n.getOwner() == file.getOwner(), "Notification owner for transfer is equivalent to original owner");
+                                Assert.assertTrue(n.getTenantId() == file.getTenantId(), "Notification tenant id for transfer is equivalent to original tenant id");
+
                                 ctx.completeNow();
                             });
                         } else {
                             ctx.failNow(response.cause());
                         }
                     });
-                } catch (JSONException e) {
-                    Assertions.fail("Unable to get JsonObject of logical file: " + e.getMessage());
+                } catch (Exception e) {
+                    ctx.failNow(e);
                 }
             }));
 
-            //Verify that the notification sent onto the queue is correct
-            List<Notification> notifications = notificationDao.getActiveForAssociatedUuidAndEvent(file.getUuid(), FileEventType.STAGING_COMPLETED.name());
-            Assert.assertFalse(notifications.isEmpty(), "No notifications found for status " + FileEventType.STAGING_COMPLETED.name());
-            Assert.assertEquals(notifications.size(), 1, "Wrong number of notifications returned for status " + FileEventType.STAGING_COMPLETED.name());
-
-            Notification n = notifications.get(0);
-            Assert.assertTrue(n.getUuid() == file.getUuid(), "Notification for transfer " + file.getTransferUuid() + " was successfully created");
-            Assert.assertTrue(n.getOwner() == file.getOwner(), "Notification owner for transfer is equivalent to original owner");
-            Assert.assertTrue(n.getTenantId() == file.getTenantId(), "Notification tenant id for transfer is equivalent to original tenant id");
+//            //Verify that the notification sent onto the queue is correct
+//            List<Notification> notifications = notificationDao.getActiveForAssociatedUuidAndEvent(file.getTransferUuid(), FileEventType.STAGING_COMPLETED.name());
+//            Assert.assertFalse(notifications.isEmpty(), "No notifications found for status " + FileEventType.STAGING_COMPLETED.name());
+//            Assert.assertEquals(notifications.size(), 1, "Wrong number of notifications returned for status " + FileEventType.STAGING_COMPLETED.name());
+//
+//            Notification n = notifications.get(0);
+//            Assert.assertTrue(n.getUuid() == file.getTransferUuid(), "Notification for transfer " + file.getTransferUuid() + " was successfully created");
+//            Assert.assertTrue(n.getOwner() == file.getOwner(), "Notification owner for transfer is equivalent to original owner");
+//            Assert.assertTrue(n.getTenantId() == file.getTenantId(), "Notification tenant id for transfer is equivalent to original tenant id");
 
 
         } catch (SchedulerException ex) {
             Assertions.fail("Enqueuing a valid staging task should not throw exception: " + ex.getMessage());
-        } catch (NotificationException e) {
-            Assertions.fail("Retrieving notification should not throw exception: " + e.getMessage());
         }
+//        catch (NotificationException e) {
+//            Assertions.fail("Retrieving notification should not throw exception: " + e.getMessage());
+//        }
     }
+
+    private JsonObject getTransferTask(LogicalFile file, String status){
+        JsonObject transferTask = new JsonObject()
+                .put("attempts", 1)
+                .put("source", file.getSourceUri())
+                .put("dest", file.getPath())
+                .put("owner", file.getOwner())
+                .put("tenantId", file.getTenantId())
+                .put("uuid", file.getTransferUuid())
+                .put("created", Instant.now())
+                .put("lastUpdated", Instant.now())
+                .put("endTime", Instant.now())
+                .putNull("parentTask")
+                .putNull("rootTask")
+                .put("status", status);
+
+        return transferTask;
+    };
 
 }
